c98c39984d163c6389b7c2b03a49818a5bfc924d
ilyas <ilyas.niyazov@rt.ru>
2022-12-24 12:29:51 +0300

    START

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,17 +2,15 @@
 Домашнее задание №1
 Функции и структуры данных
 """
-from math import ceil
 
 
-def power_numbers(*numbers):
+def power_numbers():
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
-    # >>> power_numbers(1, 2, 5, 7)
+    >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-    return [number**2 for number in numbers]
 
 
 # filter types
@@ -21,30 +19,14 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(numbers, method):
+def filter_numbers():
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
 
-    # >>> filter_numbers([1, 2, 3], ODD)
+    >>> filter_numbers([1, 2, 3], ODD)
     <<< [1, 3]
-    # >>> filter_numbers([2, 3, 4, 5], EVEN)
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-    def is_prime(number):
-        if number < 2:
-            return False
-        for i in range(2, ceil((number+1)/2)):
-            if number % i == 0:
-                return False
-        return True
-
-    if method == ODD:
-        return [i for i in numbers if i % 2 != 0]
-    elif method == EVEN:
-        return [i for i in numbers if i % 2 == 0]
-    elif method == PRIME:
-        return [i for i in numbers if is_prime(i)]
-    else:
-        return "ERROR"
