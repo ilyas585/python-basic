f929e7e9b204db164ddd6fbbd833d1de5e42bfd2
ilyas <ilyas.niyazov@rt.ru>
2022-12-24 12:33:43 +0300

    solve homework 1 - filter numbers - power numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,15 +2,17 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+from math import ceil
 
 
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
+    # >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return map(lambda x: x**2, numbers)
 
 
 # filter types
@@ -19,14 +21,30 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers, method):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
 
-    >>> filter_numbers([1, 2, 3], ODD)
+    # >>> filter_numbers([1, 2, 3], ODD)
     <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    # >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    def is_prime(number):
+        if number < 2:
+            return False
+        for i in range(2, ceil((number+1)/2)):
+            if number % i == 0:
+                return False
+        return True
+
+    if method == ODD:
+        return filter(lambda x: x % 2 != 0, numbers)
+    elif method == EVEN:
+        return filter(lambda x: x % 2 == 0, numbers)
+    elif method == PRIME:
+        return filter(lambda x: is_prime(x), numbers)
+    else:
+        return "ERROR"
